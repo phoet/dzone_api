9cee2a66fa5b2ed7caa6822ae9baad4bf301d767
phoet <phoetmail@googlemail.com>
2010-07-27 21:44:37 +0200

    create items from feed

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,22 @@ class Item < ActiveRecord::Base
     response = HTTPClient.new.get_content url
     response = response.force_encoding('UTF-8')
     hash = Crack::XML.parse response
-    mash = Hashie::Mash.new hash
+    hash['rss']['channel']['item'].map do |i|
+       item = Item.new
+       item.title = i['title']
+       item.description = i['description']
+       item.publishing_date = i['pubDate']
+       item.categories = ([] << i['category']).join(', ')
+       item.link_id = i['dz:linkId']
+       item.vote_up = i['dz:voteUpCount']
+       item.vote_down = i['dz:voteDownCount']
+       item.clicks = i['dz:clickCount']
+       item.comments = i['dz:commentCount']
+       item.thumbnail = i['dz:thumbnail']
+       item.submitter_name = i['dz:submitter']['dz:username']
+       item.submitter_image = i['dz:submitter']['dz:userimage']
+       item
+    end
   end
   
 end
--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
   test "parse_from_url" do
-    mash = Item.parse_from_url
-    assert_not_nil(mash)
+    items = Item.parse_from_url
+    assert_not_nil(items)
   end
 end
