a47f26880a03c2324150b790b4beb406fe8f2201
phoet <phoetmail@googlemail.com>
2010-07-27 18:50:54 +0200

    add item

---
+++ db/migrate/20100727165007_create_items.rb
@@ -0,0 +1,24 @@
+class CreateItems < ActiveRecord::Migration
+  def self.up
+    create_table :items do |t|
+      t.string :title
+      t.text :description
+      t.date :publishing_date
+      t.string :categories
+      t.long :link_id
+      t.integer :vote_up
+      t.integer :vote_down
+      t.integer :clicks
+      t.integer :comments
+      t.string :thumbnail
+      t.string :submitter_name
+      t.string :submitter_image
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :items
+  end
+end
