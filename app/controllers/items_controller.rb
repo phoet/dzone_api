3a5cfa885d5ea35868aee0efd934a74277d325ad
phoet <phoetmail@googlemail.com>
2010-07-30 15:33:09 +0200

    add limit and order

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   respond_to :html, :xml, :json
   
   def index
-    respond_with(@items = Item.all)
+    respond_with(@items = Item.all(:limit => 10, :order => 'id DESC'))
   end
   
 end
