6845200402aa22d966970531fb8b5e6558e9826e
phoet <phoetmail@googlemail.com>
2010-09-14 21:54:13 +0200

    add limit to query

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,8 @@ class ItemsController < ApplicationController
   respond_to :html, :xml, :json
   
   def index
-    respond_with(@items = Item.all(:limit => 25, :order => 'id DESC'))
+    limit = params[:limit] || 25
+    respond_with(@items = Item.all(:limit => limit.to_i, :order => 'id DESC'))
   end
   
   def vote
